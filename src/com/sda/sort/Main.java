cf662fa5254601aa350518f80f9665e2246d9743
tomlach <tlachawczak@gmail.com>
2018-10-21 15:43:58 +0200

    Aktutalizacja

--- src/com/sda/sort/Main.java
+++ src/com/sda/sort/Main.java
@@ -3,6 +3,27 @@ package com.sda.sort;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int tab[] = {1, 33, 32, 5, 12, 45, 342, -3, 32};
+
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
+
+        for (int i = 0; i < tab.length - 1; i++) {
+            for (int j = 0; j < tab.length - 1; j++) {
+                if (tab[j] > tab[j+1] ) {
+                    int tmp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = tmp;
+                }
+            }
+
+            System.out.println("=======================");
+
+            for (int k = 0; k<tab.length; k++) {
+                System.out.println(tab[k]);
+            }
+        }
     }
 }
